e3a0e7764c7136720a117725b77c1a4fda4acd5d
DucVu1 <141197499+DucVu1@users.noreply.github.com>
2023-08-04 13:44:21 +0700

    Update draft.c
    Add Calibration to magnetometer

--- draft.c
+++ draft.c
@@ -42,6 +42,45 @@ void mpu9250_calibrate_gyro( double x_gyror,double y_gyror, double z_gyror){
         z_gyro_calibrate_para = z_gyro_cal / count;
     }
 }
+double** mpu9250_calibrate_magneto(double x_magr,double y_magr, double z_magr){
+	 double **measurement_matrix = (double**)malloc(3 * sizeof(double*));
+	    for(int i=0;i<3;i++){
+	    measurement_matrix[i] = (double*)malloc(1 * sizeof(double));}
+
+	    // Update the array indexing and calibration values accordingly
+	    measurement_matrix[0][0] = x_magr - 0.038108;
+	    measurement_matrix[1][0] = y_magr - 0.011325;
+	    measurement_matrix[2][0] = z_magr + 0.047890;
+
+	    double **calibrated_matrix = Multiply_Mag(measurement_matrix, 3, 1, 3);
+	    freeMatrix(measurement_matrix, 3); // Free the memory allocated for measurement_matrix
+	    return calibrated_matrix;
+}
+double** Multiply_Mag(double** matrix1, int row, int column1, int column2) {
+    double calibrate_parameter[3][3] = {
+        {0.979159, 0.057901, -0.011943},
+        {0.057901, 1.010893, 0.006137},
+        {-0.011943, 0.006137, 0.996744}
+    };
+
+    // Allocate memory for the resulting matrix
+    double** multiplied_matrix = (double**)malloc(row * sizeof(double*));
+    for (int i = 0; i < row; i++) {
+        multiplied_matrix[i] = (double*)malloc(column2 * sizeof(double));
+    }
+
+    // Perform matrix multiplication
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column2; j++) {
+            multiplied_matrix[i][j] = 0;
+            for (int k = 0; k < column1; k++) {
+                multiplied_matrix[i][j] += matrix1[i][k] * calibrate_parameter[k][j];
+            }
+        }
+    }
+
+    return multiplied_matrix;
+}
 double** mpu9250_calibrate_accel(double x_accr, double y_accr, double z_accr) {
     double **measurement_matrix = (double**)malloc(3 * sizeof(double*));
     for(int i=0;i<3;i++){
@@ -149,7 +188,7 @@ void mpu9250_read(){
 	// declare variables
 	uint8_t acc_mea_x[2],acc_mea_y[2],acc_mea_z[2],gyro_mea_x[2],gyro_mea_y[2],gyro_mea_z[2];
 	uint8_t readData,mag_mea_x[2],mag_mea_y[2],mag_mea_z[2],overflow_check;
-	int16_t x_acc,z_acc,y_acc,x_gyro,y_gyro,z_gyro;
+	int16_t x_acc,z_acc,y_acc,x_gyro,y_gyro,z_gyro,x_mag,y_mag,z_mag;
 
 	 //Read data from Accelerometer and Gyroscope
 
@@ -175,7 +214,24 @@ void mpu9250_read(){
 	z_gyro = ((int16_t)gyro_mea_z[0]<<8)+gyro_mea_z[1];
 
 	HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, REG_ST1, 1, &readData, 1, General_Timeout);
+	if( (readData & 0x01) == 0x01 ){
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_X_L, 1, &mag_mea_x[0], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_X_H, 1, &mag_mea_x[1], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Y_L, 1, &mag_mea_x[0], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Y_H, 1, &mag_mea_y[1], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Z_L, 1, &mag_mea_z[0], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Z_H, 1, &mag_mea_z[1], 1, General_Timeout);
+		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, REG_ST2, 1, &overflow_check, 1, General_Timeout);
 
+		if(!(overflow_check & 0x08)) {
+		   x_mag = ((int16_t)mag_mea_x[1] << 8) | mag_mea_x[0];
+		   y_mag = ((int16_t)mag_mea_y[1] << 8) | mag_mea_y[0];
+		   z_mag = ((int16_t)mag_mea_z[1] << 8) | mag_mea_z[0];
+		}
+		else {
+			printf("\r\n");
+		}
+	}
 	//Scale to the desire (not calibrated)
 
 	double x_accr = x_acc/Scale_Constant_Acc;
@@ -185,7 +241,7 @@ void mpu9250_read(){
 	double z_gyror = z_gyro/Scale_Constant_Gyro;
 	double y_gyror = y_gyro/Scale_Constant_Gyro;
 	//Calibrate data
-
+	double **calibrated_magnetometer = mpu9250_calibrate_magneto((double)x_mag,(double)y_mag,(double)z_mag);
 	double **calibrated_accelerometer = mpu9250_calibrate_accel((double)x_accr,(double)y_accr,(double)z_accr);
 	mpu9250_calibrate_gyro(x_gyror,y_gyror,z_gyror);
 
@@ -202,30 +258,10 @@ void mpu9250_read(){
 	    printf(" %.5f   ", y_gyror);
 	    printf(" %.5f   ", z_gyror);
 	}
+    printf("Calibrated acc: %.5f  ", calibrated_magnetometer[0][0]*9.8);
+    printf(" %.5f  ", calibrated_magnetometer[1][0]*9.8);
+    printf(" %.5f    ", calibrated_magnetometer[2][0]*9.8);
 
-	if( (readData & 0x01) == 0x01 ){
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_X_L, 1, &mag_mea_x[0], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_X_H, 1, &mag_mea_x[1], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Y_L, 1, &mag_mea_y[0], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Y_H, 1, &mag_mea_y[1], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Z_L, 1, &mag_mea_z[0], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, MAG_Z_H, 1, &mag_mea_z[1], 1, General_Timeout);
-		HAL_I2C_Mem_Read(&hi2c1, (AK8963_Address<<1)+0, REG_ST2, 1, &overflow_check, 1, General_Timeout);
 
-		if(!(overflow_check & 0x08)) {
-		   double MagX = ((int16_t)rawMagData[1] << 8) | rawMagData[0];
-		   double MagY = ((int16_t)rawMagData[3] << 8) | rawMagData[2];
-		   double MagZ = ((int16_t)rawMagData[5] << 8) | rawMagData[4];
-
-			printf("Mag: %f  ", MagX);
-			printf("%f  ", MagY);
-			printf("%f\n", MagZ);
-		}
-
-	}
-	else {
-		printf("Error \r\n");
-	}
 
 }
-
